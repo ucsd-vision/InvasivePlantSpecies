2db36991bb0c3cc84ff677ecb115a3148acac9b2
r2d2bol <arflobow@gmail.com>
2017-01-22 05:47:59 -0800

    added a script to prepopulate database

---
+++ gsvannotation/src/main/python/insertpanos.py
@@ -0,0 +1,14 @@
+from subprocess import call
+# this text file contains lat/lngs for invasive species found in gsv images
+# by folks at the DEC
+# this script prepopulates the database
+f = open("species_latlngs.txt", "r")
+lines = f.readlines()
+f.close()
+
+for line in lines:
+  latlng = line[0:-1].split(',')
+  url = 'http://localhost:4567/latlng2pano?lat='+latlng[0]+'&lng='+latlng[1]
+  print url
+  call(["wget", url])
+
