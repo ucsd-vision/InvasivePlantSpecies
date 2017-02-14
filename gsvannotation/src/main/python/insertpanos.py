5f163c6728bd9f61be23c38463cbf4961df2a2f6
r2d2bol <arflobow@gmail.com>
2017-02-14 08:33:46 -0800

    replaced wget system call with call to urllib2.urlopen.  wget only works on linux platform.  urllib2 should work on all platforms

--- gsvannotation/src/main/python/insertpanos.py
+++ gsvannotation/src/main/python/insertpanos.py
@@ -1,4 +1,6 @@
 from subprocess import call
+import urllib2
+
 # this text file contains lat/lngs for invasive species found in gsv images
 # by folks at the DEC
 # this script prepopulates the database
@@ -10,5 +12,7 @@ for line in lines:
   latlng = line[0:-1].split(',')
   url = 'http://localhost:4567/latlng2pano?lat='+latlng[0]+'&lng='+latlng[1]
   print url
-  call(["wget", url])
+  #call(["wget", url])
+  f = urllib2.urlopen( url )
+  print f.read()
 
